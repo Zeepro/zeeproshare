5838376350e5fda33caa9faf06a18d85ef660168
tng <tng@4224975d-4b22-4673-a78e-9d6a9baee598>
2014-12-15 17:06:41 +0000

    changed the site's flow + logged user protection

--- application/controllers/printer.php
+++ application/controllers/printer.php
@@ -1,5 +1,8 @@
 <?php
 
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');x
+
 class Printer extends CI_Controller
 {
 	public function index()
--- application/controllers/user.php
+++ application/controllers/user.php
@@ -2,6 +2,13 @@
 
 class User extends CI_Controller
 {
+	function __construct()
+	{
+		parent::__construct();
+		if ($this->session->userdata('logged_in') == false)
+			$this->output->set_header("Location: /Login");
+	}
+
 	public function index()
 	{
 		$this->lang->load('user', $this->config->item('language'));
