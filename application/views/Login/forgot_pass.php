ae17cd6c7161f74ef23af7d8f88f376a31b900a0
zoombai <zoombai@4224975d-4b22-4673-a78e-9d6a9baee598>
2015-03-01 18:59:53 +0000

    My3DPrintWithZeepro operation

--- application/views/Login/forgot_pass.php
+++ application/views/Login/forgot_pass.php
@@ -2,18 +2,14 @@
 	<div class="ui-grid-b ui-corner-all ui-shadow ui-transparent" style="padding: 5px;">
 		<div id="error"><?php $this->load->helper('form'); echo validation_errors('<p class="zim-error">', '</p>'); ?></div>
 		<div data-role="fieldcontain">
-			<?php
-				echo form_open('/index.php/login/forgot_pass', array('data-ajax' => "false"));
-			?>
-			<input type="text" name="email" required="required" placeholder="<?php echo t('email')?>">
-			<?php
-				echo form_submit('submit', t('reset_pass'));
-				echo form_close();
-			?>
+			<form action="/login/forgot_pass" method="post" accept-charset="utf-8" data-ajax="false">
+				<input type="text" name="email" required="required" placeholder="<?php echo t('email')?>">
+				<input type="submit" name="submit" value="<?php echo t('reset_pass') ?>" data-theme="b" />
+			</form>
 		</div>
 	</div>
 <script>
-	$("header.page-header").html('<a href="javascript:history.back();" data-role="button" data-icon="back" data-ajax="false"><?php echo t('back')?></a>')
+	$("header.page-header").html('<a href="javascript:history.back();" data-role="button" data-theme="b" data-icon="back" data-ajax="false"><?php echo t('back')?></a>')
 	$("form").on("submit", function()
 	{
 		$("#overlay").addClass("gray-overlay");
--- application/views/Login/index.php
+++ application/views/Login/index.php
@@ -6,33 +6,36 @@
 			<?php
 			if (isset ( $_COOKIE ['remember_me_home_zeepro'] )) {
 				$cred = explode ( ':', $_COOKIE ['remember_me_home_zeepro'] );
-				$checked = 'checked';
+				$checked = "checked";
 			} else {
 				$cred = array (
 						"",
-						"" 
+						""
 				);
 				$checked = "";
-			}
-			echo form_open ( '/login/index', array (
-					'data-ajax' => "false" 
-			) );?>
-			<input type="text" name="email" value="<?php echo $cred [0]?>" required="required" placeholder="{email}">
-			<input type="password" name="password" value="<?php echo $cred [1]?>" required="required" placeholder="{password}">
-		    <fieldset data-role="controlgroup">
-		        <input type="checkbox" name="show_pass" id="show_pass" data-mini=true>
-		        <label for="show_pass" style="background-color: transparent;border-width:0">{show_password}</label>
-		        <input type="checkbox" name="remember" id="remember" data-mini=true <?php echo $checked?>>
-		        <label for="remember" style="background-color: transparent;border-width:0">{remember_box}</label>
-		        <input type="checkbox" name="newsletter" id="newsletter" data-mini=true>
-		        <label for="newsletter" style="background-color: transparent;border-width:0">{newsletter_box}</label>
-	    	</fieldset>
-			<?php 
-			echo form_submit ( 'submit', t ( 'signin' ) );
-			echo form_close ();
-			?>
+			}?>
+			<form action="/login/index" method="post" accept-charset="utf-8" data-ajax="false">
+				<input type="text" name="email" value="<?php echo $cred[0]?>" required="required" placeholder="{email}">
+				<input type="password" name="password" value="<?php echo $cred[1]?>" required="required" placeholder="{password}">
+			    <fieldset data-role="controlgroup">
+			        <input type="checkbox" name="show_pass" id="show_pass" data-mini="true">
+			        <label for="show_pass" style="background-color: transparent;border-width:0">{show_password}</label>
+			        <input type="checkbox" name="remember" id="remember" data-mini="true" <?php echo $checked?>>
+			        <label for="remember" style="background-color: transparent;border-width:0">{remember_box}</label>
+			        <input type="checkbox" name="optin" id="optin" data-mini="true" <?php echo "{optin}"?>>
+			        <label for="optin" style="background-color: transparent;border-width:0">{newsletter_box}</label>
+		    	</fieldset>
+				<input type="submit" name="submit" value="<?php echo t('signin')?>" data-theme="b"/>
+			</form>
 		</div>
-		<a href="/user/signup" data-role="button" data-ajax="false">{signup}</a>
+		<a href="/user/signup" data-role="button" data-theme="b" data-ajax="false">{signup}</a>
+		<br />
+		<div class="ui-grid-b ui-corner-all ui-shadow ui-transparent" style="padding: 5px; padding-top: 25px; padding-bottom: 25px; color: #000;  text-shadow: 0 1px 0 #fff">
+		{apply_text}
+		<br/>
+		<br/>
+		<a href="/my_3d_print_with_zeepro" data-role="button" data-theme="b" data-ajax="false">{apply}</a>
+		</div>		
 		<br />
 		<a href="/login/forgot_pass" data-ajax="false" style="font-weight: normal;color: #000; text-shadow: 0 1px 0 #fff">{forgot_pass}</a>
 		<br />
