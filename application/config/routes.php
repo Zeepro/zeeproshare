27f0fb8297abd6bfaaf2a2344f7af352a1ac15b0
zoombai <zoombai@4224975d-4b22-4673-a78e-9d6a9baee598>
2015-04-01 15:25:34 +0000

    3dslash

--- application/config/routes.php
+++ application/config/routes.php
@@ -45,5 +45,7 @@ $route['404_override'] = 'user';
 $route['(\w{2})/(.*)'] = '$2';
 $route['(\w{2})'] = $route['default_controller'];
 
+$route['3dslash'] = 'threedslash';
+
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
