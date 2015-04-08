8d7c810a0f008e5ad506df23ff804da4ccb6bf54
pni <pni@4224975d-4b22-4673-a78e-9d6a9baee598>
2015-04-08 14:35:34 +0000

    add - integrate apiv1 client rendering with azure blob stl fetch

--- application/views/apiv1/zim_rendering.php
+++ application/views/apiv1/zim_rendering.php
@@ -1,14 +1,3 @@
-<?php
-$template_data = array(
-		'rendering_pagetitle'	=> t('rendering_pagetitle'),
-		'reset_model_button'	=> t('reset_model_button'),
-		'request_button'		=> t('request_button'),
-		'back'					=> t('back'),
-		'msg_webgl_failed'		=> t('msg_webgl_failed'),
-		'msg_fetch_failed'		=> t('msg_fetch_failed'),
-		'rendering_stl_url'		=> '/xxx/yyy.stl',	// normally, ajax call should be in the same domain
-);
-?>
 <!-- client rendering part js -->
 <script type="text/javascript" src="/assets/rendering/ivmatrix3d.js"></script>
 <script type="text/javascript" src="/assets/rendering/ivwindow3d.js"></script>
@@ -22,46 +11,49 @@ $template_data = array(
 <style> .zeeprocanvas { background-color: rgba(0,0,0,1.0); border: 0px; } </style>
 <div class="content-wrapper">
 	<h2>{rendering_pagetitle}</h2>
-	<div id="preview_zone" style="clear: both; text-align: center;">
-		<div id="preview_image_zone"><canvas id="ivwindow3d" width="500px" height="500px" class="zeeprocanvas"></canvas></div>
-	</div>
-	<div id="detail_zone" style="clear: both; text-align: center; display: none;">
-		<div id="model_coordinate_info" style="font-size: small;">
-			X = <span id="model_xsize_info">0</span>mm x 
-			Y = <span id="model_ysize_info">0</span>mm x 
-			Z = <span id="model_zsize_info">0</span>mm
+	<div data-role="collapsible" data-collapsed="false">
+		<h3>{preview_title}</h3>
+		<div id="preview_zone" style="clear: both; text-align: center;">
+			<div id="preview_image_zone"><canvas id="ivwindow3d" width="500px" height="500px" class="zeeprocanvas"></canvas></div>
 		</div>
-		<div id="control_modify_group">
-			<div id="control_modify_mini_group" style="display: none;">
-				<div data-role="navbar" style="margin-bottom: 1em;">
-					<ul>
-						<li><a id="slicer_mini_size" href="#" onclick="javascript: onMiniSliderSwitched('s');" class="ui-btn-active">%</a></li>
-						<li><a id="slicer_mini_rotate_x" href="#" onclick="javascript: onMiniSliderSwitched('x');">X</a></li>
-						<li><a id="slicer_mini_rotate_y" href="#" onclick="javascript: onMiniSliderSwitched('y');">Y</a></li>
-						<li><a id="slicer_mini_rotate_z" href="#" onclick="javascript: onMiniSliderSwitched('z');">Z</a></li>
+		<div id="detail_zone" style="clear: both; text-align: center; display: none;">
+			<div id="model_coordinate_info" style="font-size: small;">
+				X = <span id="model_xsize_info">0</span>mm x 
+				Y = <span id="model_ysize_info">0</span>mm x 
+				Z = <span id="model_zsize_info">0</span>mm
+			</div>
+			<div id="control_modify_group">
+				<div id="control_modify_mini_group" style="display: none;">
+					<div data-role="navbar" style="margin-bottom: 1em;">
+						<ul>
+							<li><a id="slicer_mini_size" href="#" onclick="javascript: onMiniSliderSwitched('s');" class="ui-btn-active">%</a></li>
+							<li><a id="slicer_mini_rotate_x" href="#" onclick="javascript: onMiniSliderSwitched('x');">X</a></li>
+							<li><a id="slicer_mini_rotate_y" href="#" onclick="javascript: onMiniSliderSwitched('y');">Y</a></li>
+							<li><a id="slicer_mini_rotate_z" href="#" onclick="javascript: onMiniSliderSwitched('z');">Z</a></li>
+						</ul>
+						<input type="range" name="slicer_mini_control" id="slicer_mini_control" value="100" min="1" max="1000" oninput="onMiniSliderChanged(this.value);" onchange="onMiniSliderChanged(this.value);" />
+						<input type="button" class="slicer_reset_model_button" value="{reset_model_button}">
+					</div>
+				</div>
+				<div data-role="collapsible" data-collapsed="true" id="control_modify_sliders">
+					<h4>scale_rotate_title</h4>
+					<ul data-role="listview" data-inset="true">
+						<li data-role="list-divider">scale_title</li> <!-- <label for="slicer_size"></label> -->
+						<li>
+							<input type="range" name="slicer_size" id="slicer_size" value="100" min="1" max="1000" oninput="onSliderChanged('s', this.value);" onchange="onSliderChanged('s', this.value);" />
+						</li>
+						<li data-role="list-divider">rotate_title</li>
+						<li>
+							<label for="slicer_rotate_x">rotate_x_title</label>
+							<input type="range" name="slicer_rotate_x" id="slicer_rotate_x" value="0" min="-180" max="180" oninput="onSliderChanged('x', this.value);" onchange="onSliderChanged('x', this.value);" />
+							<label for="slicer_rotate_y">rotate_y_title</label>
+							<input type="range" name="slicer_rotate_y" id="slicer_rotate_y" value="0" min="-180" max="180" oninput="onSliderChanged('y', this.value);" onchange="onSliderChanged('y', this.value);" />
+							<label for="slicer_rotate_z">rotate_z_title</label>
+							<input type="range" name="slicer_rotate_z" id="slicer_rotate_z" value="0" min="-180" max="180" oninput="onSliderChanged('z', this.value);" onchange="onSliderChanged('z', this.value);" />
+						</li>
 					</ul>
-					<input type="range" name="slicer_mini_control" id="slicer_mini_control" value="100" min="1" max="1000" oninput="onMiniSliderChanged(this.value);" onchange="onMiniSliderChanged(this.value);" />
 					<input type="button" class="slicer_reset_model_button" value="{reset_model_button}">
 				</div>
-			</div>
-			<div data-role="collapsible" data-collapsed="true" id="control_modify_sliders">
-				<h4>scale_rotate_title</h4>
-				<ul data-role="listview" data-inset="true">
-					<li data-role="list-divider">scale_title</li> <!-- <label for="slicer_size"></label> -->
-					<li>
-						<input type="range" name="slicer_size" id="slicer_size" value="100" min="1" max="1000" oninput="onSliderChanged('s', this.value);" onchange="onSliderChanged('s', this.value);" />
-					</li>
-					<li data-role="list-divider">rotate_title</li>
-					<li>
-						<label for="slicer_rotate_x">rotate_x_title</label>
-						<input type="range" name="slicer_rotate_x" id="slicer_rotate_x" value="0" min="-180" max="180" oninput="onSliderChanged('x', this.value);" onchange="onSliderChanged('x', this.value);" />
-						<label for="slicer_rotate_y">rotate_y_title</label>
-						<input type="range" name="slicer_rotate_y" id="slicer_rotate_y" value="0" min="-180" max="180" oninput="onSliderChanged('y', this.value);" onchange="onSliderChanged('y', this.value);" />
-						<label for="slicer_rotate_z">rotate_z_title</label>
-						<input type="range" name="slicer_rotate_z" id="slicer_rotate_z" value="0" min="-180" max="180" oninput="onSliderChanged('z', this.value);" onchange="onSliderChanged('z', this.value);" />
-					</li>
-				</ul>
-				<input type="button" class="slicer_reset_model_button" value="{reset_model_button}">
 			</div>
 		</div>
 		<a href="#" id="request_button" class="ui-disabled" data-role="button">{request_button}</a>
